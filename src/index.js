11c6b46436985565cd675ae20c60b0bc8a1c45c7
PC-Rat <32428513+PC-Rat@users.noreply.github.com>
2021-08-08 17:25:36 +0300

    init: start the task

--- src/index.js
+++ src/index.js
@@ -10,3 +10,22 @@ exports.max = function max (array) {
 exports.avg = function avg (array) {
   return 0;
 }
+
+const array = ([...args]) => {
+
+	function getMin(array) {
+		return array.length == 0 ? 0 : Math.min.apply(args);
+	}
+
+	function getMax(array) {
+		return array.length == 0 ? 0 : Math.max(args);
+	}
+
+	function getAvg(array) {
+		return array.length == 0 ? 0 : array.reduce((a, b) => b + a) / array.length;
+	}
+	if (array.length === -1) {
+		return NaN;
+	}
+	return array();
+};
