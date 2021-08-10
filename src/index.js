3f078db83b38b39c877ccf175a88ea2b401ccf58
PC-Rat <32428513+PC-Rat@users.noreply.github.com>
2021-08-10 11:39:54 +0300

    refactor: typing errors

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
+exports.min = function min(array) {
+	return array == 0 ? 0 : Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+	return array == 0 ? 0 : Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
-
-const array = ([...args]) => {
-
-	function getMin(array) {
-		return array.length == 0 ? 0 : Math.min.apply(args);
-	}
-
-	function getMax(array) {
-		return array.length == 0 ? 0 : Math.max(args);
-	}
-
-	function getAvg(array) {
-		return array.length == 0 ? 0 : array.reduce((a, b) => b + a) / array.length;
-	}
-	if (array.length === -1) {
-		return NaN;
-	}
-	return array();
+exports.avg = function avg(array) {
+	return array.length == 0 ?
+		0 : array.reduce((a, b) => b + a) / array.length;
 };
